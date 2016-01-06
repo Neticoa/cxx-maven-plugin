c21d7825024f51769a75c24aacde3a823e4af133
fbonin <fbonin@8590dc0d-de66-4e26-86b1-74403aa5d994>
2016-01-06 11:44:06 +0000

    generateur de projet cpp
    git-svn-id: https://svn/svn/cxxplugin/trunk/cxx-maven-plugin@139 8590dc0d-de66-4e26-86b1-74403aa5d994

--- src/main/java/org/codehaus/mojo/GenerateMojo.java
+++ src/main/java/org/codehaus/mojo/GenerateMojo.java
@@ -17,6 +17,7 @@
  */
 package org.codehaus.mojo;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.text.StrSubstitutor;
 import org.apache.maven.artifact.repository.ArtifactRepository;
@@ -33,14 +34,28 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;*/
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.net.URL;
 import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Properties;
 import java.util.jar.JarFile;
 import java.util.jar.JarEntry;
 import java.util.Enumeration;
+import java.util.regex.Pattern;
+
 
 /**
  * Generates a new project from an archetype, or updates the actual project if using a partial archetype.
@@ -61,15 +76,15 @@ public class GenerateMojo
 {
 
     /**
-     * The archetype's artifactId.
-     * @parameter expression="${archetypeArtifactId}" default-value="cmake"
+     * The archetype's artifactId {cmake-cpp-project | aggregator-pom | project-parent-pom | cpp-super-pom}
+     * @parameter expression="${archetypeArtifactId}" default-value="cmake-cpp-project"
      * @since 0.0.6
      */
     //@Parameter( property = "archetypeArtifactId" )
     private String archetypeArtifactId;
 
     /**
-     * The archetype's groupId.
+     * The archetype's groupId (not used, reserved for futur usage).
      * @parameter expression="${archetypeGroupId}" default-value="org.codehaus.mojo.cxx-maven-plugin"
      * @since 0.0.6
      */
@@ -77,8 +92,8 @@ public class GenerateMojo
     private String archetypeGroupId;
 
     /**
-     * The archetype's version.
-     * @parameter expression="${archetypeVersion}" default-value=""
+     * The archetype's version (not used, reserved for futur usage).
+     * @parameter expression="${archetypeVersion}" default-value="0.1"
      * @since 0.0.6
      */
     //@Parameter( property = "archetypeVersion" )
@@ -113,10 +128,68 @@ public class GenerateMojo
     //@Parameter( defaultValue = "${project}", readonly = true )
     private MavenProject project;
     
-    protected List<String> listResourceFolderContent(String path)
+    /**
+     * The generated pom parent groupId.
+     * @parameter expression="${parentGroupId}" default-value="fr.neticoa"
+     * @required
+     * @since 0.0.6
+     */
+    private String parentGroupId;
+    
+    /**
+     * The generated pom parent artifactId.
+     * @parameter expression="${parentArtifactId}" default-value="cpp-super-pom"
+     * @required
+     * @since 0.0.6
+     */
+    private String parentArtifactId;
+    
+    /**
+     * The generated pom parent version.
+     * @parameter expression="${parentVersion}" default-value="1.0.0.0"
+     * @required
+     * @since 0.0.6
+     */
+    private String parentVersion;
+    
+    /**
+     * The generated pom groupId .
+     * @parameter expression="${groupId}" default-value="fr.neticoa"
+     * @required
+     * @since 0.0.6
+     */
+    private String groupId;
+    
+    /**
+     * The generated pom artifact Name.
+     * @parameter expression="${artifactName}"
+     * @required
+     * @since 0.0.6
+     */
+    private String artifactName;
+     
+    /**
+     * The generated pom artifactId
+     * @parameter expression="${artifactId}"
+     * @required
+     * @since 0.0.6
+     */
+    private String artifactId;
+    
+    /**
+     * The generated pom version.
+     * @parameter expression="${version}" default-value="0.0.0.1"
+     * @since 0.0.6
+     */
+    private String version;
+    
+    protected Map<String, String> listResourceFolderContent(String path, Map valuesMap)
     {
         String location = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
         final File jarFile = new File(location);
+        
+        Map resources = new HashMap<String, String>();
+        StrSubstitutor substitutor = new StrSubstitutor(valuesMap);
         
         path = (StringUtils.isEmpty(path))? "" : path + "/";
         getLog().info("listResourceFolderContent : " + location + ", sublocation : " + path);
@@ -132,7 +205,14 @@ public class GenerateMojo
                     final String name = entries.nextElement().getName();
                     if (name.startsWith(path))
                     { 
-                        getLog().info("resource entry = " + name);
+                        String resourceFile = File.separator + name;
+                        if (! (resourceFile.endsWith("/") || resourceFile.endsWith("\\")) )
+                        {
+                            getLog().info("resource entry = " + resourceFile);
+                            String destFile = substitutor.replace(resourceFile);
+                            getLog().info("become entry = " + destFile);
+                            resources.put(resourceFile, destFile);
+                        }
                     }
                 }
                 jar.close();
@@ -153,7 +233,14 @@ public class GenerateMojo
                     final File names = new File(url.toURI());
                     for (File name : names.listFiles())
                     {
-                        getLog().info("resource entry = " + name);
+                        String resourceFile = name.getPath();
+                        if (! (resourceFile.endsWith("/") || resourceFile.endsWith("\\")) )
+                        {
+                            getLog().info("resource entry = " + resourceFile);
+                            String destFile = substitutor.replace(resourceFile);
+                            getLog().info("become entry = " + destFile);
+                            resources.put(resourceFile, destFile);
+                        }
                     }
                 }
                 catch (URISyntaxException ex)
@@ -162,7 +249,7 @@ public class GenerateMojo
                 }
             }
         }
-        return null;
+        return resources;
     }
 
 
@@ -173,21 +260,6 @@ public class GenerateMojo
         //Properties userProperties = session.getUserProperties();
         Properties properties = session.getExecutionProperties();
         
-        // todo :
-        //1/ search for properties
-        // -DgroupId=fr.neticoa -DartifactName=QtUtils -DartifactId=qtutils -Dversion=1.0-SNAPSHOT
-        
-        List<String> resources = listResourceFolderContent("cmake-cxx-project");
-        
-        //2/ unpack resource to destdir 
-        //see http://www.mkyong.com/java/how-to-decompress-files-from-a-zip-file/
-        
-        //3/ create empty dir struct; if needed using a descriptor 
-        //see https://maven.apache.org/guides/mini/guide-creating-archetypes.html
-        
-        //4/ variable substitution :
-        // change prefix and suffix to '$(' and ')'
-        // see https://commons.apache.org/proper/commons-lang/javadocs/api-2.6/org/apache/commons/lang/text/StrSubstitutor.html
         /*
         $(parentGroupId)
         $(parentArtifactId)
@@ -197,5 +269,103 @@ public class GenerateMojo
         $(artifactName)
         $(version)
         */
+        Map valuesMap = new HashMap();
+        valuesMap.put("parentGroupId", parentGroupId);
+        valuesMap.put("parentArtifactId", parentArtifactId);
+        valuesMap.put("parentVersion", parentVersion);
+        valuesMap.put("groupId", groupId);
+        valuesMap.put("artifactId", artifactId);
+        valuesMap.put("artifactName", artifactName);
+        valuesMap.put("version", version);
+
+        //1/ search for properties
+        // -DgroupId=fr.neticoa -DartifactName=QtUtils -DartifactId=qtutils -Dversion=1.0-SNAPSHOT
+        
+        Map<String, String> resources = listResourceFolderContent(archetypeArtifactId, valuesMap);
+        
+
+        //2/ unpack resource to destdir 
+        //see http://www.mkyong.com/java/how-to-decompress-files-from-a-zip-file/
+        getLog().info("basdir = " + basedir);
+        //Iterator<String> itRes = resources.iterator();
+        //while( itRes.hasNext() )
+        
+        StrSubstitutor substitutor = new StrSubstitutor(valuesMap, "$(", ")");
+        
+        for (Map.Entry<String, String> entry : resources.entrySet())
+        {
+            String curRes = entry.getKey();
+            String curDest = entry.getValue();
+            InputStream resourceStream = null;
+            resourceStream = getClass().getResourceAsStream(curRes);
+            getLog().info("resource stream to open : "+ curRes);
+            getLog().info("destfile to open : "+ curDest);
+            if (null != resourceStream)
+            {
+                String sRelativePath = curDest.replaceFirst(Pattern.quote(archetypeArtifactId + File.separator), "");
+                File newFile = new File(basedir + File.separator + sRelativePath);
+                
+                
+                //3/ create empty dir struct; if needed using a descriptor 
+                //create all non exists folders
+                File newDirs = new File(newFile.getParent());
+                getLog().info("dirs to generate : "+ newDirs.getAbsoluteFile());
+                newDirs.mkdirs();
+                
+                if (! newFile.getName().equals("empty.dir"))
+                {
+                    getLog().info("file to generate : "+ newFile.getAbsoluteFile());
+                    try
+                    { 
+                        if (!newFile.createNewFile())
+                        {
+                            // duplicate existing file
+                            FileInputStream inStream = new FileInputStream(newFile);
+                            File backFile = File.createTempFile(newFile.getName(), ".back", newFile.getParentFile());
+                            FileOutputStream outStream = new FileOutputStream(backFile);
+                            
+                            IOUtils.copy( inStream, outStream );
+                            // manage file times
+                            //backFile.setLastModified(newFile.lastModified());
+                            BasicFileAttributes attributesFrom = 
+                                Files.getFileAttributeView(Paths.get(newFile.getPath()),
+                                    BasicFileAttributeView.class).readAttributes();
+                            BasicFileAttributeView attributesToView =
+                                Files.getFileAttributeView(Paths.get(backFile.getPath()),
+                                    BasicFileAttributeView.class);
+                            attributesToView.setTimes(
+                                attributesFrom.lastModifiedTime(),
+                                attributesFrom.lastAccessTime(),
+                                attributesFrom.creationTime());
+
+                            inStream.close();
+                            outStream.close();
+                        }
+                        
+                        FileOutputStream outStream = new FileOutputStream(newFile);
+                        
+                        //4/ variable substitution :
+                        // change prefix and suffix to '$(' and ')'
+                        // see https://commons.apache.org/proper/commons-lang/javadocs/api-2.6/org/apache/commons/lang/text/StrSubstitutor.html
+                        String content = IOUtils.toString(resourceStream, "UTF8");
+                        content = substitutor.replace(content);
+
+                        //IOUtils.copy( resourceStream, outStream );
+                        IOUtils.write(content, outStream, "UTF8");
+
+                        outStream.close();
+                        resourceStream.close();
+                    }
+                    catch ( IOException e )
+                    {
+                        getLog().error( "File " + newFile.getAbsoluteFile() + " can't be created : " + e);
+                    }
+                }
+            }            
+        }
+        
+
+        
+
     }
 }
